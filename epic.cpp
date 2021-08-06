aec94e669970913847ef41ab113bba741ea54159
ICan'tReadYourMind <onjishnuahasan103@gmail.com>
2021-08-06 19:30:36 +0600

    aaa

--- epic.cpp
+++ epic.cpp
@@ -1,23 +1,8 @@
-//trying to learn c++
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    unsigned long long int x;
-
-    for( ; ; ) {
-    std::cout << "Insert number (insert 0, text or Control+C to exit): ";
-    std::cin >> x;
-
-    if (x == 0) {
-        std::cout << "You inserted a number or text, the application will now close.";
-        return 0;
-    }
-    else if (x % 2 > 0) {
-        std::cout << "The number is odd.\n";
-    }
-    else if (x % 2 == 0) {
-        std::cout << "The number is even.\n";
-    }
-}
+    cout << "Hello World!";
 }
